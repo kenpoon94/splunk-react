6b0ea5d99d55965306ab940208ba0898293220d4
Ken Poon <kpoon@mypassglobal.com>
2023-03-07 21:50:55 +0800

    Refactor RQScroll

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,39 @@
 import "./App.css";
-import { Flex, Container, Center } from "@chakra-ui/react";
+import { Box, Text, Flex, Container, Center, Stack } from "@chakra-ui/react";
 import Pagination from "./components/Pagination";
 import { Hide } from "./components/Hide";
 import Nav from "./components/Nav";
+import { useCallback, useRef } from "react";
+import usePosts from "./hooks/usePosts";
+import { PostI } from "./interfaces/interface";
+import Post from "./components/Post";
 
 const HomePage = () => {
+  const {
+    data: posts,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = usePosts();
+
+  const intersectionObserver = useRef<any>(null);
+  const lastPostRef = useCallback(
+    (post: any) => {
+      if (isFetchingNextPage) return;
+      if (intersectionObserver.current)
+        intersectionObserver.current.disconnect();
+
+      intersectionObserver.current = new IntersectionObserver((posts) => {
+        if (posts[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+
+      if (post) intersectionObserver.current.observe(post);
+    },
+    [isFetchingNextPage, fetchNextPage, hasNextPage]
+  );
+
   return (
     <Container>
       <Nav />
@@ -15,7 +44,22 @@ const HomePage = () => {
               <Pagination />
             </Hide>
           </Center>
-          <Center>{/* TODO: <Posts /> */}</Center>
+          <Center>
+            <Box>
+              {isFetchingNextPage && <Text>Loading more posts...</Text>}
+              <Stack spacing="4">
+                {posts?.pages.map((page: any) => {
+                  return page.map((post: PostI, i: any) => {
+                    if (page.length === i + 1)
+                      return (
+                        <Post ref={lastPostRef} key={post.id} post={post} />
+                      );
+                    return <Post key={post.id} post={post} />;
+                  });
+                })}
+              </Stack>
+            </Box>
+          </Center>
         </Container>
       </Flex>
     </Container>
--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useInfiniteQuery } from "react-query";
 import { getPostsPage } from "../api/axios";
 
 export type PostI = {
@@ -7,38 +8,16 @@ export type PostI = {
   title: string;
 };
 
-const usePosts = (page = 1) => {
-  const [results, setResults] = useState<PostI[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [error, setError] = useState<{ message: string } | {}>({});
-  const [hasNextPage, setHasNextPage] = useState(false);
+const usePosts = () => {
+  const getPosts: any = async ({ pageParam = 1 }) => {
+    return await getPostsPage(pageParam);
+  };
 
-  useEffect(() => {
-    setIsLoading(true);
-    setIsError(false);
-    setError({});
-
-    const controller = new AbortController();
-    const { signal } = controller;
-
-    getPostsPage(page, { signal })
-      .then((data) => {
-        setResults((prev: any) => [...prev, ...data]);
-        setHasNextPage(Boolean(data.length));
-        setIsLoading(false);
-      })
-      .catch((e) => {
-        setIsLoading(false);
-        if (signal.aborted) return;
-        setIsError(true);
-        setError({ message: e.message });
-      });
-
-    return () => controller.abort();
-  }, [page]);
-
-  return { isLoading, isError, error, results, hasNextPage };
+  return useInfiniteQuery(["posts"], getPosts, {
+    getNextPageParam: (lastPage: any[], allPages: any[]) => {
+      return lastPage.length ? allPages.length + 1 : undefined;
+    },
+  });
 };
 
 export default usePosts;
